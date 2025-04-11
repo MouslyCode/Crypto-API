52f90b0e887abd7e6268cd451d5e44d9b615b38b
MouslyCode <rezasubkhan50@gmail.com>
2025-04-11 19:58:47 +0700

    file structure

---
+++ app/src/main/java/com/mouscode/cryptocurrency/common/Constant.kt
@@ -0,0 +1,4 @@
+package com.mouscode.cryptocurrency.common
+
+object Constant {
+}
---
+++ app/src/main/java/com/mouscode/cryptocurrency/common/Resource.kt
@@ -0,0 +1,8 @@
+package com.mouscode.cryptocurrency.common
+
+sealed class Resource<T>(val data: T? = null,val message: String? = null) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Failure<T>(message: String,data: T? = null): Resource<T>(data,message)
+    class Loading<T>(data: T): Resource<T>(data)
+
+}
